594bb325daad28febfb0e28f27b8b82002839b26
ParaN3xus <paran3xus007@gmail.com>
2024-01-12 17:30:33 +0800

    feat: uint_linked_list

---
+++ src/uint_linked_list/uint_linked_list.cpp
@@ -0,0 +1,75 @@
+#include <stdexcept>
+#include "uint_linked_list.h"
+
+
+uint_linked_list::uint_linked_list() : head(nullptr) {}
+
+uint_linked_list::~uint_linked_list() {
+    while (head != nullptr) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+void uint_linked_list::append(unsigned int data) {
+    Node* newNode = new Node{data, nullptr};
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Node* current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+void uint_linked_list::insert(int index, unsigned int data) {
+    if (index < 0) return;
+    Node* newNode = new Node{data, nullptr};
+    if (index == 0) {
+        newNode->next = head;
+        head = newNode;
+    } else {
+        Node* current = head;
+        for (int i = 0; i < index - 1 && current != nullptr; i++) {
+            current = current->next;
+        }
+        if (current != nullptr) {
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+}
+
+void uint_linked_list::remove(unsigned int data) {
+    if (head == nullptr) return;
+    if (head->data == data) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+    Node* current = head;
+    while (current->next != nullptr && current->next->data != data) {
+        current = current->next;
+    }
+    if (current->next != nullptr) {
+        Node* temp = current->next;
+        current->next = current->next->next;
+        delete temp;
+    }
+}
+
+unsigned int& uint_linked_list::operator[](int index) {
+    Node* current = head;
+    for (int i = 0; i < index && current != nullptr; i++) {
+        current = current->next;
+    }
+    if (current != nullptr) {
+        return current->data;
+    } else {
+        throw std::out_of_range("Index out of range");
+    }
+}
---
+++ src/uint_linked_list/uint_linked_list.h
@@ -0,0 +1,23 @@
+#ifndef UINT_LINKED_LIST_H
+#define UINT_LINKED_LIST_H
+
+class uint_linked_list {
+private:
+    struct Node {
+        unsigned int data;
+        Node* next;
+    };
+
+    Node* head;
+
+public:
+    uint_linked_list();
+    ~uint_linked_list();
+
+    void append(unsigned int data);
+    void insert(int index, unsigned int data);
+    void remove(unsigned int data);
+    unsigned int& operator[](int index);
+};
+
+#endif // UINT_LINKED_LIST_H
