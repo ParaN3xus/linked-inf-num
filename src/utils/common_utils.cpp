42016b7e198191867b8dd32a03d89a0845b35ff2
ParaN3xus <paran3xus007@gmail.com>
2024-01-12 18:53:38 +0800

    feat: infNumStr2BinStr

---
+++ src/utils/common_utils.cpp
@@ -0,0 +1,22 @@
+#include "common_utils.h"
+
+std::string infNumStr2BinStr(std::string num) {
+    std::string binary;
+    while (num != "0") {
+        binary = ((num.back() - '0') % 2 ? "1" : "0") + binary;
+
+        // div
+        int carry = 0;
+        for (int i = 0; i < num.size(); ++i) {
+            int current = num[i] - '0' + carry * 10;
+            num[i] = current / 2 + '0';
+            carry = current % 2;
+        }
+
+        // remove leading zeros
+        while (num.size() > 1 && num[0] == '0') {
+            num.erase(num.begin());
+        }
+    }
+    return binary;
+}
---
+++ src/utils/common_utils.h
@@ -0,0 +1,8 @@
+#ifndef COMMON_UTILS_H
+#define COMMON_UTILS_H
+
+#include<string>
+
+std::string infNumStr2BinStr(std::string num);
+
+#endif // COMMON_UTILS_H
