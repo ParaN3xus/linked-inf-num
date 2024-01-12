c665131f24b3e04fd950acffd54b7450eafa7bab
ParaN3xus <paran3xus007@gmail.com>
2024-01-12 21:02:39 +0800

    feat: init inf_int

--- src/inf_int/inf_int.cpp
+++ src/inf_int/inf_int.cpp
@@ -1,1 +1,29 @@
 #include "inf_int.h"
+#include "../uint_linked_list/uint_linked_list.cpp"
+
+inf_int::inf_int(std::string num) {
+    if (num[0] == '-') {
+        sign = true;
+        num.erase(num.begin());
+    }
+    else {
+        sign = false;
+    }
+
+    digits.append(0);
+
+    int cnt = 0;
+    std::string binary = infNumStr2BinStr(num);
+    for (int i = binary.size() - 1; i >= 0; --i) {
+        digits[0] += (binary[i] - '0') << cnt;
+        cnt++;
+        if (cnt == INF_INT_DIGIT_SIZE) {
+            digits.insert(0, 0);
+            cnt = 0;
+        }
+    }
+}
+
+inf_int::~inf_int() {
+}
+
--- src/inf_int/inf_int.h
+++ src/inf_int/inf_int.h
@@ -1,4 +1,18 @@
 #ifndef INF_INT_H
 #define INF_INT_H
 
+#include <string>
+#include "../utils/common_utils.h"
+#include "../uint_linked_list/uint_linked_list.h"
+
+class inf_int {
+private:
+    bool sign;
+    uint_linked_list digits;
+public:
+    inf_int(std::string num);
+    ~inf_int();
+};
+
+
 #endif // INF_INT_H
