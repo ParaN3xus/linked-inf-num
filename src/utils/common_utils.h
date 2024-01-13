19fc2fb4751d6bf0ebe68159833f3ead8e84782d
ParaN3xus <paran3xus007@gmail.com>
2024-01-13 09:35:03 +0800

    fix: ensure correct define

--- src/utils/common_utils.h
+++ src/utils/common_utils.h
@@ -15,6 +15,6 @@ std::string binstr_add1(const std::string& num);
 std::string add_commas(const std::string& input);
 
 const int INF_INT_DIGIT_SIZE = sizeof(int) * 8;
-#define UINT_MAX INF_INT_DIGIT_SIZE == 32 ? UINT32_MAX : UINT64_MAX
+#define UINT_MAX ((INF_INT_DIGIT_SIZE == 32) ? UINT32_MAX : UINT64_MAX)
 
 #endif // COMMON_UTILS_H
