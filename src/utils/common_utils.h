0d4991005af9a7e8ee013fd2c80bcce6e4b0aeb7
ParaN3xus <paran3xus007@gmail.com>
2024-01-13 08:26:37 +0800

    fix: missing definitions of utils

--- src/utils/common_utils.h
+++ src/utils/common_utils.h
@@ -5,6 +5,10 @@
 
 std::string inf_num_str2bin_str(std::string num);
 
+bool is_add_overflow(unsigned int a, unsigned int b, bool carry);
+bool is_add_overflow(unsigned int a, unsigned int b);
+bool is_add_overflow(unsigned int a, bool carry);
+
 std::string binstr_mut2(const std::string& num);
 std::string binstr_add1(const std::string& num);
 
