714e920eef8513d3085afaa7ac6939bd4ab6d4af
ParaN3xus <paran3xus007@gmail.com>
2024-01-29 09:34:09 +0800

    feat: universial header

---
+++ src/linked_inf_num.h
@@ -0,0 +1,9 @@
+#ifndef LINKED_INF_NUM_H
+#define LINKED_INF_NUM_H
+
+#include "utils/common_utils.h"
+#include "uint_linked_list/uint_linked_list.h"
+#include "inf_int/inf_int.h"
+#include "inf_float/inf_float.h"
+
+#endif // LINKED_INF_NUM_H
