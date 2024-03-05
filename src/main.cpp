ce07728f492412a90db756a46304acd6b3fed126
ParaN3xus <paran3xus007@gmail.com>
2024-03-05 15:54:55 +0800

    feat: calculate pi

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,45 @@
 #include "linked_inf_num.h"
+#include <chrono>
 #include <iostream>
 
+const int PRECISION = 2;
+const int ITER = 100000;
+const int OUTPUT_PRECISION = 30;
 
 int main() {
-    linked_inf_num::inf_int a("-12345678901234567890123456789012345678901234567890"), b("98765432198765432198765432198765432198765432198765");
-    linked_inf_num::inf_float c("-1234567890123456789012345.6789012345678901234567890", 3), d("9876543219876543219876543.2198765432198765432198765", 3);
+    linked_inf_num::inf_float res("3", PRECISION);
 
-    std::cout << a + b << std::endl << a - b << std::endl << a * b << std::endl << a / b << std::endl;
-    std::cout << c + d << std::endl << c - d << std::endl << c * d << std::endl << c / d << std::endl;
-    std::cout << (linked_inf_num::inf_float::div(c, d, 3)) << std::endl;
+    linked_inf_num::inf_float dividend("4", PRECISION);
+
+    linked_inf_num::inf_float divisor_a("2", PRECISION);
+    linked_inf_num::inf_float divisor_b("3", PRECISION);
+    linked_inf_num::inf_float divisor_c("4", PRECISION);
+
+    linked_inf_num::inf_float two("2", PRECISION);
+
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < ITER; i++) {
+        if (i % 2 == 0) {
+            res = res + dividend / (divisor_a * divisor_b * divisor_c);
+        }
+        else {
+            res = res - dividend / (divisor_a * divisor_b * divisor_c);
+        }
+
+        divisor_a = divisor_a + two;
+        divisor_b = divisor_b + two;
+        divisor_c = divisor_c + two;
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+
+    std::cout << res.to_string(false, OUTPUT_PRECISION, linked_inf_num::ROUND_TO_NEAREST) << std::endl;
+
+    std::cout << "Time taken: " << diff.count() << "s" << std::endl;
+
 
     return 0;
 }
